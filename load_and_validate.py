introduction of validation loop, resulting in batch-averaged loss of 9e-4, but something is obviously wrong here, as the autoencoder is not outputting images that resemble the initial inputs.

load_and_validate.py
@@ -4,15 +4,26 @@
 
 import torch
 from autoencoder import Autoencoder
-from test_and_train.test import test
+from data import load_MNIST
+from train_and_validate.validate import validate
 
 def main(model_path:str="./trained_models/autoencoder.pt"):
 
     loaded_model = Autoencoder()
     loaded_model.load_state_dict(torch.load(model_path))
 
-    numel_list = [p.numel() for p in loaded_model.parameters()]
-    print(numel_list)
+    #Get data about the number of parameters in the model.
+    # numel_list = [p.numel() for p in loaded_model.parameters()]
+    # print(numel_list)
+
+    _, val_loader = load_MNIST()
+
+    mean_val_loss = validate(
+        model=loaded_model,
+        val_loader=val_loader
+        )
+    
+    print(mean_val_loss.detach())
 
 
 if __name__ == "__main__":

main.py
@@ -6,7 +6,7 @@ import torch
 import torch.optim as optim
 from data import load_MNIST
 from autoencoder import Autoencoder
-from test_and_train.train import train
+from train_and_validate.train import train
 
 
 #Prevent access errors when trying to load the MNIST database

train_and_validate/train.py
@@ -22,10 +22,10 @@ def train(n_epochs:int, model:nn.Module, optimizer:optim.Optimizer, train_loader
         for imgs, labels in train_loader:
 
             batch_size = imgs.shape[0]
-            img_batch = imgs.view(batch_size, -1)
+            inputs = imgs.view(batch_size, -1)
             
-            outputs = model(img_batch)
-            loss = model.loss_fn(outputs, img_batch) #calculate the BCE Loss
+            outputs = model(inputs)
+            loss = model.loss_fn(outputs, inputs) #calculate the BCE Loss
 
             optimizer.zero_grad() #prevent gradient accumulation
             loss.backward() #backpropagation on the model

train_and_validate/validate.py
@@ -6,7 +6,7 @@ from data import load_MNIST
 
 #Need to think a little more about what I should be doing with this.
 
-def test(model:nn.Module, val_loader:DataLoader, generation_no:int)->torch.Tensor:
+def validate(model:nn.Module, val_loader:DataLoader)->float:
     """
     Tests the output of the autoencoder.
 
@@ -16,24 +16,28 @@ def test(model:nn.Module, val_loader:DataLoader, generation_no:int)->torch.Tenso
         generation_no (int): The number of images we want the model to generate.
 
     Returns:
-        output (torch.Tensor): The generated output in tensor form.
+        mean_val_loss: The total loss across the validation set, divided by the number of images in the validation set.
 
     """
 
-    output_list = []
+    loss_val = 0.0
+    total = 0
 
     with torch.no_grad():
         for val_images, _ in val_loader:
-            no_per_batch = int(generation_no/len(val_images))
 
-            for img in val_images[:no_per_batch]:
-                output = model(img)
+            batch_size = val_images.shape[0] #get the size of the batch.
+            total += batch_size
 
+            inputs = val_images.view(batch_size, -1)
+            
+            outputs = model(inputs)
+            loss_val += model.loss_fn(outputs, inputs)
 
-        # image = image.view(-1, 784)
-        # output = model(image)
-        # output_list.append(output)
+    mean_val_loss = loss_val / total
 
-    output_stack = torch.cat(output_list, dim=0)
+    return mean_val_loss
 
-    return output_stack
+            
+
+        