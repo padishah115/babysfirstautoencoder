First successful run of autoencoder. Fixed redundant normalisation.

autoencoder.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Autoencoder(nn.Module):
     """
@@ -12,41 +13,19 @@ class Autoencoder(nn.Module):
     """
 
     def __init__(self, outer_chans=784, central_chans=16):
-        """
-        Initialise the Autoencoder class for training on the MNIST database of handwritten digits.
-
-        Attributes:
-            self.outer_chans (int): The number of input features at the initial FC layer, and the number of output features at the final FC layer.
-            self.central_chans (int): The number of input and output features at the central layer.
-        """
-
-
         super().__init__()
-        self.outer_chans = outer_chans
-        self.central_chans = central_chans
+        self.outer_chans = outer_chans #Number of pixels in flattened image tensor
+        self.central_chans = central_chans #Number of channels in the central layer
 
-        self.input_layer = nn.Linear(in_features=self.outer_chans, out_features=self.central_chans)
-        self.central_layer = nn.Linear(in_features=self.central_chans, out_features=self.central_chans)
-        self.output_layer = nn.Linear(in_features=self.central_chans, out_features=self.outer_chans)
-        self.activation = nn.Sigmoid()
+        self.encoder = nn.Linear(in_features=self.outer_chans, out_features=self.central_chans)
+        self.decoder = nn.Linear(in_features=self.central_chans, out_features=self.outer_chans)
 
         #use binary cross entropy loss function for autoencoder training.
-        self.loss_fn = nn.BCELoss()
+        self.criterion = nn.MSELoss()
 
 
     def forward(self, x:torch.Tensor)->torch.Tensor:
-        """
-        Execute the forwards pass through the network.
-
-        Args:
-            x (torch.Tensor): Input tensor from the MNIST database, of shape (batch size, 784)
-
-        Returns:
-            out (torch.Tensor): The output of the network (i.e. best attempt to generate a similar image)
-        """
-
-        out = self.activation(self.input_layer(x))
-        out = self.activation(self.central_layer(out))
-        out = self.activation(self.output_layer(out))
-        return out
+        x = F.relu(self.encoder(x)) #Use the relu activation function for the output from the encoder
+        x = F.sigmoid(self.decoder(x)) #Use the sigmoid activation function on the final images as they are normalized to between 0, 1
+        return x
     

main.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from data import load_MNIST
 from autoencoder import Autoencoder
 from train_and_validate.train import train
+from train_and_validate.load_and_validate import load_and_validate
 
 
 #Prevent access errors when trying to load the MNIST database
@@ -17,14 +18,15 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def main():
 
     data_path = './data'
-    train_loader, val_loader = load_MNIST(data_path=data_path, normalise=True) #pixels normalised to between 0 and 1
+    train_loader, val_loader = load_MNIST(data_path=data_path, batch_size=256) 
 
     #Load autoencoder model
     myAutoencoder = Autoencoder()
-    n_epochs = 100
-    learning_rate = 1e-2
-    optimizer = optim.SGD(params=myAutoencoder.parameters(), lr=learning_rate)
+    n_epochs = 30
+    learning_rate = 1e-3
+    optimizer = optim.Adam(params=myAutoencoder.parameters(), lr=learning_rate)
 
+    #Training loop
     train(
         n_epochs=n_epochs,
         model=myAutoencoder,
@@ -32,8 +34,12 @@ def main():
         train_loader=train_loader
     )
 
+    #Save the model
     save_path = './trained_models/'
     torch.save(myAutoencoder.state_dict(), save_path + 'autoencoder.pt')
+
+    #Load the model and validate
+    load_and_validate()
 
 
 if __name__ == '__main__':

train_and_validate/load_and_validate.py
@@ -7,7 +7,12 @@ from autoencoder import Autoencoder
 from data import load_MNIST
 from train_and_validate.validate import validate
 
-def main(model_path:str="./trained_models/autoencoder.pt"):
+def load_and_validate(model_path:str="./trained_models/autoencoder.pt"):
+    """
+    
+    Args:
+        model_path (str): The path to the model which we want to load
+    """
 
     loaded_model = Autoencoder()
     loaded_model.load_state_dict(torch.load(model_path))
@@ -24,7 +29,3 @@ def main(model_path:str="./trained_models/autoencoder.pt"):
         )
     
     print(mean_val_loss.detach())
-
-
-if __name__ == "__main__":
-    main()

train_and_validate/train.py
@@ -2,8 +2,9 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from autoencoder import Autoencoder
 
-def train(n_epochs:int, model:nn.Module, optimizer:optim.Optimizer, train_loader:DataLoader):
+def train(n_epochs:int, model:Autoencoder, optimizer:optim.Optimizer, train_loader:DataLoader):
     """
     Performs training loops over specified number of epochs using desired optimizer.
 
@@ -15,6 +16,8 @@ def train(n_epochs:int, model:nn.Module, optimizer:optim.Optimizer, train_loader
 
     """
 
+    criterion = model.criterion #Loss function for the model
+
     for epoch in range(1, n_epochs+1):
         
         loss_train = 0.0
@@ -25,13 +28,13 @@ def train(n_epochs:int, model:nn.Module, optimizer:optim.Optimizer, train_loader
             inputs = imgs.view(batch_size, -1)
             
             outputs = model(inputs)
-            loss = model.loss_fn(outputs, inputs) #calculate the BCE Loss
+            loss = criterion(outputs, inputs) #calculate the BCE Loss
 
             optimizer.zero_grad() #prevent gradient accumulation
             loss.backward() #backpropagation on the model
             optimizer.step()
             
-            loss_train += loss
+            loss_train += loss.item()
 
         if epoch == 1 or epoch % 10 == 0:
             print(f"Mean batch loss at epoch {epoch}", loss_train/len(train_loader)) #print the batch-normalised loss at epoch 1 and every 10th epoch

train_and_validate/validate.py
@@ -2,11 +2,13 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
+from autoencoder import Autoencoder
+
 from data import load_MNIST
 
 #Need to think a little more about what I should be doing with this.
 
-def validate(model:nn.Module, val_loader:DataLoader)->float:
+def validate(model:Autoencoder, val_loader:DataLoader)->float:
     """
     Tests the output of the autoencoder.
 
@@ -32,7 +34,7 @@ def validate(model:nn.Module, val_loader:DataLoader)->float:
             inputs = val_images.view(batch_size, -1)
             
             outputs = model(inputs)
-            loss_val += model.loss_fn(outputs, inputs)
+            loss_val += model.criterion(outputs, inputs)
 
     mean_val_loss = loss_val / total
 